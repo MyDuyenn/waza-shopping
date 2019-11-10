e262c4d46c874c28cc1e49b88ef14c69fe184955
Duyên Nguyễn <mduyen021298@gmail.com>
2019-11-10 21:54:17 +0700

    first commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Router, Redirect } from "@reach/router";
+import { userLogout } from "../AuthUser/action";
+import { connect } from "react-redux";
+import Navbar from "../Navbar";
+import MainHome from "./MainHome";
+import ShopDetail from "../ShopDetail";
+
+class Home extends Component {
+  render() {
+    if (!this.props.token) {
+      console.log("here");
+      return <Redirect to="/auth" noThrow />;
+    }
+    return (
+      <div>
+        <Navbar user={this.props.user} />
+        <Router>
+          <MainHome path="/" />
+          <ShopDetail path="/shop/:id" />
+        </Router>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  token: state.authUser.token,
+  user: state.authUser.user
+})
+
+const mapDispatchToProps = dispath => {
+  return {
+    userLogout: () => dispath(userLogout())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
